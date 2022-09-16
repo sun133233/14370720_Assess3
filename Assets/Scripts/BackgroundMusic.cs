446877a1b78c0cce721eb7522fa011cf2487fd9d
Liangyi Wang <sun133233@gmail.com>
2022-09-16 14:49:17 +0800

    music submit

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class BackgroundMusic : MonoBehaviour
+{
+    public AudioClip BM_intro;
+    public AudioClip BM_normal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(playSound());
+    }
+    IEnumerator playSound()
+    {
+        GetComponent<AudioSource>().clip = BM_intro;
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(BM_intro.length);
+        GetComponent<AudioSource>().clip = BM_normal;
+        GetComponent<AudioSource>().Play();
+        GetComponent<AudioSource>().loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
