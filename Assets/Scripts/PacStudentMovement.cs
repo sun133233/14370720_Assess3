530c92d84b946e2d872e9f47fbfbe2f4c7894ea2
Liangyi Wang <sun133233@gmail.com>
2022-09-21 13:55:44 +0800

    85%

--- Assets/Scripts/PacStudentMovement.cs
+++ Assets/Scripts/PacStudentMovement.cs
@@ -4,39 +4,49 @@ using UnityEngine;
 
 public class PacStudentMovement : MonoBehaviour
 {
+    private AudioSource PacSource;
     private Animator PacController;
+    private string[] triggers = {"P_RIGHT", "P_DOWN", "P_LEFT","P_UP" };
     public Transform PacStudent;
-    float duration = 3.0f;
+    float duration = 2.0f;
     float timeOut = 0.0f;
+    Vector3[] positions = { new Vector3(-12.0f, 13.0f, 0.0f), new Vector3(-7.0f, 13.0f, 0.0f), new Vector3(-7.0f, 9.0f, 0.0f), new Vector3(-12.0f, 9.0f, 0.0f) };
+    public int currentPos;
     // Start is called before the first frame update
     void Start()
     {
         PacController = GetComponent<Animator>();
-        PacStudent.transform.position = new Vector3(-4.0f, 0.0f, 0.0f);
+        PacStudent.transform.position = new Vector3(-12.0f, 13.0f, 0.0f);
+        PacSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && timeOut<duration)
+        if (timeOut<duration)
         {
-            PacStudent.transform.position = Vector3.Lerp(new Vector3(-4.0f, 0.0f, 0.0f), new Vector3(-4.0f, 3.0f, 0.0f), timeOut / duration);
+            int nextPos = currentPos + 1;
+            
+            if (nextPos == positions.Length)
+            {
+                nextPos = 0;
+            }
+            PacStudent.transform.position = Vector3.Lerp(positions[currentPos], positions[nextPos], timeOut / duration);
             timeOut += Time.deltaTime;
+            PacController.SetTrigger(triggers[currentPos]);
+    }
+        else
+        {
+            timeOut = 0.0f;
+            currentPos += 1;
+            if (currentPos == positions.Length)
+            {
+                currentPos = 0;
+            }
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) && timeOut < duration)
+        if (!PacSource.isPlaying)
         {
-            PacStudent.transform.position = Vector3.Lerp(new Vector3(-4.0f, 3.0f, 0.0f), new Vector3(5.0f, 3.0f, 0.0f), timeOut / duration);
-            timeOut += Time.deltaTime;
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow) && timeOut < duration)
-        {
-            PacStudent.transform.position = Vector3.Lerp(new Vector3(5.0f, 3.0f, 0.0f), new Vector3(5.0f, -4.0f, 0.0f), timeOut / duration);
-            timeOut += Time.deltaTime;
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && timeOut < duration)
-        {
-            PacStudent.transform.position = Vector3.Lerp(new Vector3(5.0f, -4.0f, 0.0f), new Vector3(-4.0f, -4.0f, 0.0f), timeOut / duration);
-            timeOut += Time.deltaTime;
+            PacSource.Play();
         }
     }
 }
